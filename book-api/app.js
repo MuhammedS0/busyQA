7167221b6e00882c1e8a123532c2cfbc0a2d4d68
MuhammedS0 <51326559+MuhammedS0@users.noreply.github.com>
2024-10-06 17:19:08 -0400

    Add files via upload

---
+++ book-api/app.js
@@ -0,0 +1,20 @@
+//app.js
+
+const express = require('express');
+const app = express();
+const port = 3000;
+// Require the books module
+const books = require('./books'); 
+
+//Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a route to get all books
+app.get('/api/books', (req, res) => {
+    res.json(books);
+});
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
